d1f409be4cc52a067f3a137f658b64458b672ddb
Jae-Yi <plotojjy@gmail.com>
2022-11-23 09:19:07 +0900

    J2 로그인 페이지 UI commit

--- src/pages/Login.js
+++ src/pages/Login.js
@@ -1,31 +1,46 @@
-import "../styles/Login.scss"
+import "../styles/Login.scss";
+import { FaUser, FaLock } from "react-icons/fa";
+import { SiKakaotalk, SiGoogle, SiGithub } from "react-icons/si";
 
 function Login() {
-
   return (
     <div class="container">
       <div class="screen">
         <div class="screen__content">
           <form class="login">
             <div class="login__field">
-              <i class="login__icon fas fa-user"></i>
-              <input type="text" class="login__input" placeholder="User name / Email" />
+              <FaUser className="login__icon" />
+              <input
+                type="text"
+                class="login__input"
+                placeholder="User name / Email"
+              />
             </div>
             <div class="login__field">
-              <i class="login__icon fas fa-lock"></i>
-              <input type="password" class="login__input" placeholder="Password" />
+              <FaLock className="pwd__icon" />
+              <input
+                type="password"
+                class="login__input"
+                placeholder="Password"
+              />
             </div>
             <button class="button login__submit">
               <span class="button__text">Log In Now</span>
               <i class="button__icon fas fa-chevron-right"></i>
             </button>
           </form>
-          <div class="social-login">
+          <div className="social-login">
             <h3>log in via</h3>
-            <div class="social-icons">
-              <a href="#" class="social-login__icon fab fa-instagram"></a>
-              <a href="#" class="social-login__icon fab fa-facebook"></a>
-              <a href="#" class="social-login__icon fab fa-twitter"></a>
+            <div className="social-icons">
+              <a href="#" className="kakaoIcon">
+                <SiKakaotalk />
+              </a>
+              <a href="#" className="googleIcon">
+                <SiGoogle />
+              </a>
+              <a href="#" className="githubIcon">
+                <SiGithub />
+              </a>
             </div>
           </div>
         </div>
